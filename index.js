6291ca6471c6b7d190264cacae2970967958cc95
ian <ianmwendamicheni@gmail.com>
2023-09-15 08:23:13 +0300

    complete assignment

---
+++ index.js
@@ -0,0 +1,11 @@
+
+function introduction(Ianoh) {
+    return `Hi, my name is ${Ianoh}.`;
+  }
+  function introductionWithLanguage(name, language) {
+    return `Hi, my name is ${name} and I am learning to program in ${language}.`;
+  }
+  function introductionWithLanguageOptional(name, language = "JavaScript") {
+    return `Hi, my name is ${name} and I am learning to program in ${language}.`;
+  }
+  
